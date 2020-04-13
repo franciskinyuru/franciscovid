29c8df665a844eadd25ce6648b8226e514132030
francis warui <franciskinyuru26@gmail.com>
2020-04-13 04:50:03 +0000

    check

--- src/estimator.php
+++ src/estimator.php
@@ -1,6 +1,6 @@
 <?php
  function Covid19ImpactEstimator(){
-    	$data_input='{"region": {"name": "Africa","avgAge": 19.7,"avgDailyIncomeInUSD": 4,"avgDailyIncomePopulation": 0.73},"periodType": "days","timeToElapse": 38,"reportedCases": 2747,"population": 92931687,"totalHospitalBeds": 678874}';
+$data_input='{"region": {"name": "Africa","avgAge": 19.7,"avgDailyIncomeInUSD": 4,"avgDailyIncomePopulation": 0.73},"periodType": "days","timeToElapse": 38,"reportedCases": 2747,"population": 92931687,"totalHospitalBeds": 678874}';
 $datas=json_decode($data_input);
 $data=array("data"=>$datas);
 $rry=array("estimate"=>array_merge(impact($datas),severe($datas)));
@@ -31,8 +31,6 @@ return $xml_info->asXML();
  $ke=json_decode($final,true);*/
 return $result;
 }
-
- }
 function impact($datas){
 	$currenlyInfected=(int)$datas->reportedCases*(10);
 	$factor=(int)(((int)$datas->timeToElapse)/3);
